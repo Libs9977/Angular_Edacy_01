70b80aa5f4fe02e0b20c75748f4ecfd548eb2761
Libasse Mbaye <libasslaye@gmail.com>
2024-03-27 06:26:36 +0200

    Creation of SecondComponent

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SecondComponentComponent } from '../second-component/second-component.component';
 
 @Component({
   selector: 'app-first',
   standalone: true,
-  imports: [],
+  imports: [SecondComponentComponent],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
 })
